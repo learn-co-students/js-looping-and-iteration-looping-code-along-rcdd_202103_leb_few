758fdeda68ff20f1991f0d07a6e318b38bb2ed0b
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-12 09:49:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, gName){
+  let msg = [];
+  for(let i = 0; i < names.length; i++) {
+    msg.push(`Thank you, ${names[i]}, for the wonderful ${gName} gift!`)
+  }
+  return msg;
+}
+writeCards(['Mohamad','Salim','Abdualla'], 'car');
+
+
+function countDown(num) {
+
+  for(let i = num; i > -1; i--){
+    console.log(i);
+  }
+
+}
+countDown(4);
