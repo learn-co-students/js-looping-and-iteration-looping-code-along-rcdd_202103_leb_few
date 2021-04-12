cd7db8d143c1181438e5eb7143cc643fa2a57c38
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-04-12 01:23:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function countDown(a){
+  for(let i =a;i>=0;i--){
+    console.log(i);
+  }
+}
+let messages = [];
+let wtf = ['adnan','samir'];
+let e = 'birthday';
+let names = [];
+function writeCards(names,eventName){
+  for (let i = 0; i < names.length; i++) {
+messages[i]=(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+
+  }
+  return messages;
+}
+countDown(10);
+writeCards(wtf,e);
