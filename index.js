ea08e794dac8f6ddf158adee850c1bfedb069715
Nour baltaji <nourbaltaji99@gmail.com>
2021-04-12 14:47:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(gifts, eventName){
+  let newArray = [];
+  for (let i = gifts.length-1; i >= 0; i--) {
+    newArray.unshift(`Thank you, ${gifts[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return newArray;
+}
+// console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+function countDown(posNum){
+  while (posNum >= 0) {
+    console.log(posNum);
+    posNum--
+  }
+}
