3d41cbd01b56be3481bd60af83e48545cc6f8cc5
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-04-13 20:52:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+//No.1
+
+
+
+function writeCards(names , birthday) {
+  const heyHey = []
+  for (let i = 0; i < names.length; i++) {
+heyHey.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+}
+
+return heyHey;
+}
+let names = ["Ada", "Brendan", "Ali"];
+writeCards(names, 'birthday');
+
+
+
+
+//No.2
+let number = 15;
+
+function countDown(number) {
+while (number >= 0) {
+console.log(number--);
+}
+
+return countDown;
+}
+
+countDown(number);
